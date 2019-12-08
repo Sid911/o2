669d783e5e2f943fd65d56cbc09e660c3223c08b
Siddharth Sinha <siddharthsinha9752@gmail.com>
2019-12-08 22:44:48 +0530

    added news api and bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { createStore} from "redux";
 import {Provider} from 'react-redux'
 import { BrowserRouter as Router,Route,Link } from "react-router-dom";
-import "../assets/css/argon-dashboard-react.min.css"
+import "./assets/css/argon-dashboard-react.min.css"
 
 import TitleBar from './Titlebar/titlebar'
 import HomePage from "./Pages/Dashboard/Home/Home"
