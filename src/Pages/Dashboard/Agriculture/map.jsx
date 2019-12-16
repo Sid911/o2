07ad37dca7292d20e59a58d68053d99d9877bf92
PRIYANSH <chromiumcromy@gmail.com>
2019-12-16 16:55:40 +0530

    c

---
+++ src/Pages/Dashboard/Agriculture/map.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card, CardImg} from "reactstrap";
+import M from "../../../assets/ulalala.jpg";
+
+export default function Agriculture() {
+    return (
+        <div>
+            <Card>
+                <CardImg src={M} alt="...">
+                </CardImg>
+                <CardBody>
+                    
+                </CardBody>
+            </Card>
+       </div>
+    )
+}
