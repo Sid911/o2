5a995c678386d3ff9ee0d16ae00dc9d21d956d13
Goku-pr <priyanshusahu91@gmail.com>
2019-12-17 05:11:02 +0530

    vbv

--- src/Pages/Dashboard/Home/WeatherCard.jsx
+++ src/Pages/Dashboard/Home/WeatherCard.jsx
@@ -9,8 +9,14 @@ export default function WeatherCard() {
     var weather = useSelector(state=>state.weather.data)
     console.log(weather)
     return (
+        <div>
         <Card className="shadow" style={{padding:"10%"}}>
             {weather.name}
+            {weather.main.temp}
+            {weather.main.feels_like}
+            {weather.main.temp_max}
+            {weather.main.temp_min}
         </Card>
+        </div>
     )
 }
