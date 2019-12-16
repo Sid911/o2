b87e941d30af6f274ad14c21539728dc33cdebf2
PRIYANSH <chromiumcromy@gmail.com>
2019-12-16 23:35:56 +0530

    c

---
+++ src/Pages/Dashboard/Agriculture/healthcare.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Button,ButtonGroup,Card,CardImg,CardBody,CardTitle,CardText} from "reactstrap";
+
+
+export default function Agriculture() {
+    return (
+        <div>
+             <Card style={{ width: "26rem" }}>
+                <CardImg alt="..." src={M} top />
+                <CardBody>
+                    <CardTitle>Card title</CardTitle>
+                    <CardText>
+                        Some quick example text to build on the card title and make up
+                        the bulk of the card's content.
+                    </CardText>
+                    <Button color="primary" href="#pablo" onClick={e=> e.preventDefault()}
+                        >
+                        Go somewhere
+                    </Button>
+                </CardBody>
+            </Card> 
+        </div>
+        )
+        }
