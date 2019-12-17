c38f742fb3b34127c227faebcda9b8dbe9e37787
Goku-pr <priyanshusahu91@gmail.com>
2019-12-17 08:17:51 +0530

    dsf

--- src/Pages/Maps.jsx
+++ src/Pages/Maps.jsx
@@ -1,12 +1,89 @@
-import {GoogleApiWrapper} from 'google-maps-react';
- import React from 'react'
-// ...
- 
-export class MapContainer extends React.Component {}
-const LoadingContainer = (props) => (
-    <div>Fancy loading container!</div>
-  )
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBRbGfNhsLe8wgQD1Gnm5RHUZqk38J1au4"),
-  LoadingContainer: LoadingContainer
-})(MapContainer)
+import React from "react";
+// react plugin used to create google maps
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const MapWrapper = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
+      defaultOptions={{
+        scrollwheel: false,
+        styles: [
+          {
+            featureType: "administrative",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#444444" }]
+          },
+          {
+            featureType: "landscape",
+            elementType: "all",
+            stylers: [{ color: "#f2f2f2" }]
+          },
+          {
+            featureType: "poi",
+            elementType: "all",
+            stylers: [{ visibility: "off" }]
+          },
+          {
+            featureType: "road",
+            elementType: "all",
+            stylers: [{ saturation: -100 }, { lightness: 45 }]
+          },
+          {
+            featureType: "road.highway",
+            elementType: "all",
+            stylers: [{ visibility: "simplified" }]
+          },
+          {
+            featureType: "road.arterial",
+            elementType: "labels.icon",
+            stylers: [{ visibility: "off" }]
+          },
+          {
+            featureType: "transit",
+            elementType: "all",
+            stylers: [{ visibility: "off" }]
+          },
+          {
+            featureType: "water",
+            elementType: "all",
+            stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
+          }
+        ]
+      }}
+    >
+      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
+    </GoogleMap>
+  ))
+);
+
+class Maps extends React.Component {
+  render() {
+    return (
+      <>
+        <MapWrapper
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRbGfNhsLe8wgQD1Gnm5RHUZqk38J1au4"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={
+            <div
+              style={{ height: `600px` }}
+              className="map-canvas"
+              id="map-canvas"
+            />
+          }
+          mapElement={
+            <div style={{ height: `100%`, borderRadius: "inherit" }} />
+          }
+        />
+      </>
+    );
+  }
+}
+
+export default Maps;
